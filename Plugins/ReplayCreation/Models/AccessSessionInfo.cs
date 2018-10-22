c5a887d4d411881988ad46cd3f31850e4658589a
Eugene Orlianskii <eorlianskii@tableau.com>
2018-10-22 09:00:11 -0700

    Added JsonProperty to all props going into output json

--- Plugins/ReplayCreation/Models/AccessSessionInfo.cs
+++ Plugins/ReplayCreation/Models/AccessSessionInfo.cs
@@ -8,13 +8,13 @@ namespace Logshark.Plugins.ReplayCreation.Models
     /// </summary>
     internal class AccessSessionInfo
     {
-        public string apache_request_id { get; set; }
-        public string request_method { get; set; }
-        public string request_ip { get; set; }
-        public string resource { get; set; }
-        public DateTime request_time { get; set; }
-        public string load_time { get; set; }
-        public string status_code { get; set; }
+        public string ApacheRequestID { get; set; }
+        public string RequestMethod { get; set; }
+        public string RequestID { get; set; }
+        public string Resource { get; set; }
+        public DateTime RequestTime { get; set; }
+        public string LoadTime { get; set; }
+        public string StatusCode { get; set; }
 
         /// <summary>
         /// Parse logLine and extract required data from it
@@ -22,20 +22,20 @@ namespace Logshark.Plugins.ReplayCreation.Models
         /// <param name="logLine"></param>
         public AccessSessionInfo(BsonDocument logLine)
         {
-            apache_request_id = logLine.GetValue("request_id").AsString;
-            request_method = logLine.GetValue("request_method").AsString;
-            request_ip = logLine.GetValue("request_ip").AsString;
-            resource = logLine.GetValue("resource").AsString;
-            load_time = logLine.GetValue("request_time").AsString;
+            ApacheRequestID = logLine.GetValue("request_id").AsString;
+            RequestMethod = logLine.GetValue("request_method").AsString;
+            RequestID = logLine.GetValue("request_ip").AsString;
+            Resource = logLine.GetValue("resource").AsString;
+            LoadTime = logLine.GetValue("request_time").AsString;
             var offset = logLine.GetElement("ts_offset").Value.ToString();
             var timeOffSet = int.Parse(offset);
             var ts = new System.TimeSpan(timeOffSet / 100, timeOffSet % 100, 0);
 
             //convert the current time to UTC
             var browserStartTime = (DateTime)logLine.GetElement("ts").Value - ts;
-            request_time = browserStartTime.ToUniversalTime();
+            RequestTime = browserStartTime.ToUniversalTime();
 
-            status_code = logLine.GetValue("status_code").AsString;
+            StatusCode = logLine.GetValue("status_code").AsString;
         }
     }
 }
