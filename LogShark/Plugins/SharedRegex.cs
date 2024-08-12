eb9761cbd915d1b522a04968cfb931836f0dab16
JOSH ROOD <jrood@nhaschools.com>
2024-08-12 16:44:48 -0400

    In 2024.2 java logs, a new id is added directly following ts_offset. Regex is adjusted to optionally pull in id (assumed to be PID) and successfully parse 2024.2+ logs.

--- LogShark/Plugins/SharedRegex.cs
+++ LogShark/Plugins/SharedRegex.cs
@@ -4,9 +4,11 @@ namespace LogShark.Plugins
 {
     public static class SharedRegex
     {
+        // 2024.2 added optional "pid" to match 2024.2, and made ts_offset more exclusionary
         public static readonly Regex JavaLogLineRegex = new Regex(@"^
             (?<ts>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3})\s
-            (?<ts_offset>[^\s]+?)\s
+            (?<ts_offset>[-|+]\d+)\s
+            ?(?<pid>\d+)?\s
             \((?<site>[^,]*?), (?<user>[^,]*?), (?<sess>[^,]*?), (?<req>[^,\)]*?) (,(?<local_req_id>[^\)]*?))?\)\s
             (?<thread>[^\s]*?)\s
              (?<service>[^:]*?)?:\s
